b353d8c49db45a79d90228fecfa381c21b9d87f2
Jesús Antonio Ugaz Vegas <jesusugazv@gmail.com>
2022-05-27 16:26:48 -0500

    Finish

--- src/store/index.js
+++ src/store/index.js
@@ -2,8 +2,19 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    todos:[
+      { id:'1', text:'Recolectar las piedras del infinito', completed: false },
+      { id:'2', text:'Piedra del alma', completed: true },
+      { id:'3', text:'Piedra del poder', completed: true },
+      { id:'4', text:'Piedra de realidad', completed: false },
+      { id:'5', text:'Conseguir nuevos secuaces competentes', completed: false }
+    ] 
   },
   getters: {
+    pendingTodos( state, getters) {
+      return state.todos.filter(todo => !todo.completed)
+    }
+
   },
   mutations: {
   },
---
+++ src/views/TodoVuex.vue
@@ -0,0 +1,35 @@
+<template>
+    <h1>Lista de tareas de Thanos</h1>
+
+    <!-- <h4>Pendientes: {{ $store.state.todos.filter(t => !t.completed).length }} </h4> -->
+    <h4>Pendientes: {{ pending.length }}</h4>
+
+    <hr>
+
+
+</template>
+
+<script>
+import { computed } from 'vue'
+import { useStore } from 'vuex'
+
+export default {
+
+    setup() {
+
+        const store = useStore()
+
+
+
+        return {
+            pending: computed(() => store.getters['pendingTodos'])
+        }
+    }
+
+
+}
+</script>
+
+<style>
+
+</style>
